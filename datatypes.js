d263aef7734214f80ea8a758139885fcb9acfcf2
Bhavadharani-14 <easwaramoorthybhavadharani@gmail.com>
2026-06-13 14:17:23 +0530

    added variables.js

---
+++ datatypes.js
@@ -0,0 +1,46 @@
+console.log("Wlecome to primitive datatype");
+//numbers
+console.log("Dt numbers");
+var age = 30;
+console.log(age);
+
+//string
+console.log("Dt string");
+var name = "deepika";
+console.log(name);
+
+//boolean 
+console.log("Dt boolean");
+var isStudent = true;
+console.log(isStudent);
+
+//null
+console.log("Dt null");
+var emptyvalue = null;
+console.log(emptyvalue);
+
+//undefine
+console.log("Dt undefined");
+var undefined;
+console.log(undefined);  
+
+//symbols 
+console.log("Dt symbols");
+var symbolvalue = Symbol('symbol');
+console.log(symbolvalue);  
+
+
+console.log(Wellcome to non-primitive datatype);
+//ojects
+console.log("Dt object");
+var person = {
+    name:"deepika"
+    age:18
+};
+console.log(person.age);
+
+//Arrays
+console.log("Dt arrays");
+var numbers = c;symbolvalue["Phone,battery,watch,Mouse,moniter"];
+console.log(Number.lenght);
+console.log(numbers[4]);
--- looping.js
+++ looping.js
@@ -1,3 +1,2 @@
-for (let i =1;i<=10;i++){
-    console.log(i + "x 3 =" + i*3 );
-}
+//for
+for(let i=1; )
