a30e8b9aff33671da988d2143f810539879dfff9
Bhavadharani-14 <easwaramoorthybhavadharani@gmail.com>
2026-06-13 14:32:41 +0530

    added function.js

---
+++ function.js
@@ -0,0 +1,11 @@
+// Non parametric function
+function greek(){
+    console.log("Hello, world!");
+}
+greek(); // output:Hello,world!
+
+// paramatric function
+function add(a,b){
+    return a + b;
+}
+console.log(add(5,3));
