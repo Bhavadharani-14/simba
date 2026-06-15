9eae9bd45a1aba50ee1b344fee4cd1784fdf9fe3
Bhavadharani-14 <easwaramoorthybhavadharani@gmail.com>
2026-06-15 11:01:48 +0530

    added looping.js

--- looping.js
+++ looping.js
@@ -1,2 +1,23 @@
 //for
-for(let i=1; )
+for(let i=1;i<10;i++)
+{
+    console.log(i+"x 2 ="+i*2);
+    
+}
+
+
+//while loop
+let i = 1
+while(i<=10)
+{
+    console.log(i+"x 3 =" i*3 )
+    i++;
+}
+
+//do while loop
+let count=1
+do{
+    console.log("count");
+    count++;
+}
+while(count<=10)
