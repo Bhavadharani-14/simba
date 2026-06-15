e4c8120c5516223c468f21480210c34be5aa574e
Bhavadharani-14 <easwaramoorthybhavadharani@gmail.com>
2026-06-15 12:03:32 +0530

    added es6-example.js

--- es6-example.js
+++ es6-example.js
@@ -7,3 +7,46 @@ console.log(pi);
 //template literals
 let age=24
 console.log(`my name is $ {name} my age is${pi} `);
+
+//object
+const student=
+{
+    name: "Bhava",
+    college: "VETIAS"
+}
+    console.log(student);
+
+//destructing
+const person={
+    dept : "ai&ds",
+    gender : "male"
+}    
+const{dept,gender}=person
+console.log(dept);
+
+
+
+//arrow function
+let add=(a,b) => a+b;
+console.log(add(10,2));
+    
+
+//array
+let fruits = ["apple","orange","grapes"]
+console.log(fruits);
+
+//sperad operator
+const updatedfruits =[
+    ...fruits,
+    "stabbary"
+];
+console.log(updatedfruits);
+
+//rest oprator
+function maximarks(...marks)
+{
+    let max = Math.max(...marks)
+    return max
+}
+console.log("maximarks:",maximarks(80,90,70));
+
